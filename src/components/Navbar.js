e685e353db5f6c43cb0c22cab53fcffcdcfd574d
pazlort <pazmarialort@gmail.com>
2022-10-24 00:37:29 -0300

    PreEntrega1GrechtLort

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,65 @@
 import logo from '../img/logo.png';
 import CartWidget from "./CartWidget.js";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavbarBootstrap from 'react-bootstrap/Navbar';
 
-let desplegable = document.querySelector("#navbarSupportedContent");
-
-function toggleMenu() {
-   if (desplegable.classList.value === 'collapse navbar-collapse show' ) {
-    desplegable.classList.value = 'collapse navbar-collapse';
-   } else if (desplegable.classList.value === 'collapse navbar-collapse' ) {
-    desplegable.classList.value = 'collapse navbar-collapse show';
-   };
-};
-
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <button className="navbar-toggler collapsed" type="button" onClick={toggleMenu} data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <a className="navbar-brand" href="/">
+    <NavbarBootstrap bg="light" expand="lg">
+      <Container>
+        <NavbarBootstrap.Brand href="#home">
           <img src={logo} alt="Logo" width="200" height="70" className="d-inline-block align-text-top"></img>
-        </a>
-        <div className="collapse navbar-collapse show" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Velas y bengalas</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">Globos</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">Banderines</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">Adornos para tortas</a>
-            </li>
-          </ul>
-        </div>
-        <CartWidget/>
-      </div>
-    </nav>
+        </NavbarBootstrap.Brand>
+        <NavbarBootstrap.Toggle aria-controls="basic-Navbar-nav" />
+        <NavbarBootstrap.Collapse id="basic-Navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#velasVengalas">Velas y bengalas</Nav.Link>
+            <Nav.Link href="#globos">Globos</Nav.Link>
+            <Nav.Link href="#banderines">Banderines</Nav.Link>
+            <Nav.Link href="#adornosTortas">Adornos para tortas</Nav.Link>
+          </Nav>
+        </NavbarBootstrap.Collapse>
+      </Container>
+      <CartWidget/>
+    </NavbarBootstrap>
   );
-};
+}
 
 export default Navbar;
+
+
+
+// const Navbar = () => {
+//   return (
+//     <nav className="Navbar Navbar-expand-lg bg-light">
+//       <div className="container-fluid d-flex flex-row">
+//         <button className="Navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="/NavbarSupportedContent" aria-controls="NavbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+//           <span className="Navbar-toggler-icon"></span>
+//         </button>
+//         <a className="Navbar-brand" href="/">
+//           <img src={logo} alt="Logo" width="200" height="70" className="d-inline-block align-text-top"></img>
+//         </a>
+//         <div className="collapse Navbar-collapse show" id="NavbarSupportedContent">
+//           <ul className="Navbar-nav me-auto mb-2 mb-lg-0">
+//             <li className="nav-item">
+//               <a className="nav-link" href="/">Velas y bengalas</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="/">Globos</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="/">Banderines</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="/">Adornos para tortas</a>
+//             </li>
+//           </ul>
+//         </div>
+//        
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
